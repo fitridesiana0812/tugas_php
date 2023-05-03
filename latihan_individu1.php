913acb4f044143bfebda55942cd9a913776af6e5
fitridesiana0812 <126475945+fitridesiana0812@users.noreply.github.com>
2023-05-03 19:43:11 +0700

    Update latihan_individu1.php

--- latihan_individu1.php
+++ latihan_individu1.php
@@ -1,13 +1,10 @@
 <?php
-    // deklarasi array bilangan
-	$bilangan = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
-
-	// looping untuk mengecek setiap bilangan dalam array
-	for($i = 0; $i < count($bilangan); $i++){
-		if($bilangan[$i] % 2 == 0){ // jika bilangan genap
-			echo $bilangan[$i] . " adalah bilangan genap <br>";
+	// looping untuk mengecek setiap bilangan dalam angka
+	for($i = 0; $i < 10; $i++){
+		if($i % 2 == 0){ // jika bilangan genap
+			echo "$i adalah bilangan genap <br>";
 		}else{ // jika bilangan ganjil
-			echo $bilangan[$i] . " adalah bilangan ganjil <br>";
+			echo "$i adalah bilangan ganjil <br>";
 		}
 	}
 ?>

6a74b64ca4e328377d618678c3aaeaed20ed827a
fitridesiana0812 <126475945+fitridesiana0812@users.noreply.github.com>
2023-05-03 20:17:56 +0700

    Update latihan_individu2.php

--- latihan_individu2.php
+++ latihan_individu2.php
@@ -4,20 +4,24 @@
 </head>
 <body>
 	<table border="1">
-  <tr>
-    <th>No</th>
-    <th>Nama</th>
-    <th>Kelas</th>
-  </tr>
-  <?php
-    for($i=1; $i<=10; $i++){ //looping untuk 10 baris
-        echo "<tr>";
-        echo "<td>".$i."</td>"; //tampilkan nomor
-        echo "<td>Nama ke ".$i."</td>"; //tampilkan nama ke-i
-        echo "<td>Kelas ".(11-$i)."</td>"; //tampilkan kelas
-        echo "</tr>";
-    }
-  ?>
+        <tr style="background-color: blue;">
+            <th>No</th>
+            <th>Nama</th>
+            <th>Kelas</th>
+        </tr>
+    <?php
+        for($i=1; $i<=10; $i++){ //looping untuk 10 baris
+            if ($i % 2 == 0) {
+                echo "<tr>";
+            } else {
+                echo "<tr style='background-color: gray;'>";
+            }
+            echo "<td>".$i."</td>"; //tampilkan nomor
+            echo "<td>Nama ke ".$i."</td>"; //tampilkan nama ke-i
+            echo "<td>Kelas ".(11-$i)."</td>"; //tampilkan kelas
+            echo "</tr>";
+        }
+    ?>
 </table>
 </body>
 </html>

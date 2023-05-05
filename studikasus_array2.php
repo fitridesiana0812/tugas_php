cce5ceb15262ba939a2b5a81f8041c265f8a9412
fitridesiana0812 <126475945+fitridesiana0812@users.noreply.github.com>
2023-05-05 23:26:37 +0700

    Delete studikasus_array2.php

--- studikasus_array2.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // membuat array nama
-    $nama = array("Pelita", "Najmina", "Anita", "Bayu", "Nasa", "Rahma");
-
-    // menampilkan data pada array
-    for($i=0; $i<count($nama); $i++) {
-    echo "Nama : ".$nama[$i]."<br>";
-    }
-?>
